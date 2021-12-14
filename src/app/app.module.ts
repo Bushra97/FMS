8047406eb9dcb1eee76512b277d2694f77158caa
Bushra <bushratasneem97@gmail.com>
2021-12-15 04:32:28 +0530

    home page created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
+// import {AppRoutingModule} from './app-routing.module';
+
 @NgModule({
   declarations: [
     AppComponent,
